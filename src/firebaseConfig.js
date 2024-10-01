7951ba7c560e840205bbebd2ccacf4a5a3387710
Michael Zhao <michaelzhao314@gmail.com>
2024-10-01 10:52:52 -0500

    Fixed firebase config spacing

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "API KEY",
-    authDomain: "joshenem-75746.firebaseapp.com",
-    projectId: "joshenem-75746",
-    storageBucket: "joshenem-75746.appspot.com",
-    messagingSenderId: "1054602828415",
-    appId: "1:1054602828415:web:41fb1a2721a00a8fb0fd4a",
-    measurementId: "G-0DDNF7JHZB"
-  };
+  apiKey: "API KEY",
+  authDomain: "joshenem-75746.firebaseapp.com",
+  projectId: "joshenem-75746",
+  storageBucket: "joshenem-75746.appspot.com",
+  messagingSenderId: "1054602828415",
+  appId: "1:1054602828415:web:41fb1a2721a00a8fb0fd4a",
+  measurementId: "G-0DDNF7JHZB"
+};
 
-  const app = initializeApp(firebaseConfig);
-  
-  export const db = getFirestore(app);
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
